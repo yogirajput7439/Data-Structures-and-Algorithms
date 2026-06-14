30fe83ff3cbb4143bdc3e674aecce58533c6c9d6
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-06-14 23:01:31 +0530

    Add initial structure for Missing_Numbers.cpp

---
+++ Missing_Numbers.cpp
@@ -0,0 +1,14 @@
+// Best Solution 
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans ^= i ^ nums[i];
+        }
+
+        return ans;
+    }
+};
