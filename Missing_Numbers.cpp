b5f8f949d74875cd2f4ae08c4d0a61e765958bcd
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-06-14 23:06:34 +0530

    Add optimal solution for finding missing number

--- Missing_Numbers.cpp
+++ Missing_Numbers.cpp
@@ -12,3 +12,21 @@ public:
         return ans;
     }
 };
+
+// Optimal Solution 
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        long long expected = 1LL * n * (n + 1) / 2;
+        long long actual = 0;
+
+        for (int num : nums) {
+            actual += num;
+        }
+
+        return expected - actual;
+    }
+};
