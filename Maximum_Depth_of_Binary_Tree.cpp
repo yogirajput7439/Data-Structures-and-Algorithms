6ab58e9beb278765e609415f4df67a2fb6394b76
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-06-15 22:11:37 +0530

    Implement iterative maxDepth function for binary tree

--- Maximum_Depth_of_Binary_Tree.cpp
+++ Maximum_Depth_of_Binary_Tree.cpp
@@ -19,3 +19,33 @@ public:
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
+// solutions
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            while (size--) {
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if (curr->left) q.push(curr->left);
+                if (curr->right) q.push(curr->right);
+            }
+
+            depth++;
+        }
+
+        return depth;
+    }
+};
