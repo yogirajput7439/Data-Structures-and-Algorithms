29df3f7f6ae7c121ad76ef6482c595f3ba13d305
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-09-06 19:04:12 +0530

    Implement optimized two-sum solution
    Added an optimized approach for the two-sum problem using a hash map.

--- 01_ARRAY/Two_Sum.cpp
+++ 01_ARRAY/Two_Sum.cpp
@@ -1,4 +1,4 @@
-// BRUTE FORCE APROACH ) --- 
+// 1 BRUTE FORCE APROACH ) --- 
 
 class Solution {
 public:
@@ -18,3 +18,27 @@ public:
     }
 };
 
+// 2 OPTIMEZED APPROACH --
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < n; i++){
+            int need = target - nums[i];
+
+            if(mp.find(need) != mp.end()){
+                return {mp[need], i};
+
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
